109c7f7ca533082d55d19c9eea63165e7ecc5e31
Juniar Rakhman <jrakhman@skynet.local>
2023-08-22 10:10:04 +0700

    added cybu and surround

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,6 +2,7 @@ return {
   -- Add the community repository
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.note-taking.obsidian-nvim" },
   -- disable this when working with vue
   -- { import = "astrocommunity.pack.typescript" },
