4e8e2dbf1c16ba165a16ebbca32bd6b9a0f8d99f
Juniar Rakhman <juniar@almende.org>
2024-05-06 20:08:49 +0700

    update

--- lua/community.lua
+++ lua/community.lua
@@ -4,7 +4,8 @@ return {
   { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.lsp.lsp-inlayhints-nvim" },
   { import = "astrocommunity.lsp.lsp-signature-nvim" },
-  -- { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
 
   { import = "astrocommunity.pack.java" },
   -- { import = "astrocommunity.pack.vue" },
