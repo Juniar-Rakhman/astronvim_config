d66e12394501309befd650bac47412d1b65a56be
Juniar Rakhman <juniar@almende.org>
2025-04-30 21:43:21 +0700

    fixing cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -2,11 +2,15 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    opts = {
-      preselect = require("cmp").PreselectMode.Item,
-      completion = {
-        completeopt = "menu,menuone,noinsert",
-      },
-    },
+    opts = function(_, opts)
+      local cmp = require "cmp"
+      opts.sources = cmp.config.sources {
+        { name = "nvim_lsp", priority = 1000 },
+        { name = "luasnip", priority = 750 },
+        { name = "buffer", priority = 500 },
+        { name = "path", priority = 250 },
+      }
+      return opts
+    end,
   },
 }
