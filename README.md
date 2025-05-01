9d372b4303d066c4d8abbffcfc9862995fffb945
Juniar Rakhman <juniar@almende.org>
2025-05-01 16:31:23 +0700

    init V5

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ A template for getting started with [AstroNvim](https://github.com/AstroNvim/Ast
 ```shell
 mv ~/.config/nvim ~/.config/nvim.bak
 mv ~/.local/share/nvim ~/.local/share/nvim.bak
-mv /.local/state/nvim ~/.local/state/nvim.bak
+mv ~/.local/state/nvim ~/.local/state/nvim.bak
 mv ~/.cache/nvim ~/.cache/nvim.bak
 ```
 
--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,3 +1,5 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
