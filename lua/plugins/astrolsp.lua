9b2cf27fb495d93218029cb0178ec22d2c4a4550
Juniar Rakhman <juniar.rahman@gmail.com>
2025-05-22 12:50:23 +0700

    meh

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -24,7 +24,6 @@ return {
           -- "python",
         },
       },
-      -- TODO: do something
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         "lua_ls",
