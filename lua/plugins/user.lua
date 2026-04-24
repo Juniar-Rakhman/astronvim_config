254db3bba7208533ec8fe87f476603277fe1dd57
Juniar Rahman <juniar@almende.org>
2026-04-24 15:07:33 +0700

    update to nvim 0.12, disable aerial

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -3,4 +3,5 @@ return {
   -- You can disable default plugins as follows:
   { "max397574/better-escape.nvim", enabled = false },
   { "s1n7ax/nvim-window-picker", enabled = false },
+  { "stevearc/aerial.nvim", enabled = false },
 }
