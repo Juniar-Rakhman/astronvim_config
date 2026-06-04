c29ac6c32f9e49b2eeb20cb3d3f32bff6baad90f
Juniar Rahman <juniar@almende.org>
2026-06-04 10:06:29 +0700

    revert to catpuccin, everforest can't dim well

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -7,7 +7,7 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    colorscheme = "everforest",
+    colorscheme = "catppuccin-frappe",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
